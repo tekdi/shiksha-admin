4aac675b5a68efd58ba8b6089cabdca93c5d1693
itsvick <vivek_kasture@techjoomla.com>
2024-11-29 16:53:15 +0530

    Issue #PS-0000 fix: Updated sidebar menus

--- src/components/layouts/sidebar/MenuItems.js
+++ src/components/layouts/sidebar/MenuItems.js
@@ -12,7 +12,7 @@ const Menuitems = [
   {
     title: "SIDEBAR.CENTERS",
     icon: centerIcon,
-    href: "/centers",
+    href: ["/centers"],
   },
   {
     title: "SIDEBAR.MANAGE_USERS",
@@ -20,15 +20,15 @@ const Menuitems = [
     subOptions: [
       {
         title: "SIDEBAR.TEAM_LEADERS",
-        href: "/team-leader",
+        href: ["/team-leader"],
       },
       {
         title: "SIDEBAR.FACILITATORS",
-        href: "/faciliator",
+        href: ["/faciliator"],
       },
       {
         title: "SIDEBAR.LEARNERS",
-        href: "/learners",
+        href: ["/learners"],
       },
     ],
   },
@@ -38,15 +38,15 @@ const Menuitems = [
     subOptions: [
       {
         title: "MASTER.STATE",
-        href: "/state",
+        href: ["/state"],
       },
       {
         title: "MASTER.DISTRICTS",
-        href: "/district",
+        href: ["/district"],
       },
       {
         title: "MASTER.BLOCKS",
-        href: "/block",
+        href: ["/block"],
       },
     ],
   },
@@ -55,7 +55,7 @@ const Menuitems = [
         {
           title: "SIDEBAR.COURSE_PLANNER",
           icon: coursePlannerIcon,
-          href: "/course-planner",
+          href: ["/course-planner", "/stateDetails", "/subjectDetails", "/importCsv", "/resourceList"],
         },
       ]
     : []),
@@ -64,7 +64,7 @@ const Menuitems = [
         {
           title: "SIDEBAR.WORKSPACE",
           icon: dashboardIcon,
-          href: "/workspace/content/create",
+          href: ["/workspace/content/create"],
         },
       ]
     : []),
