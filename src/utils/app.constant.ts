61d666970fc5d911414624cf4cb5acf00562a6f1
ttpl-rt-217 <shreyas_s@techjoomla.com>
2024-12-01 14:39:59 +0530

    admin UI enchancement

--- src/utils/app.constant.ts
+++ src/utils/app.constant.ts
@@ -1,3 +1,24 @@
+import { TFunction } from 'i18next';
+import pdf from "./../assets/images/PDF.svg";
+import epub from "../assets/images/Epub.svg";
+import html from "../assets/images/HTML.svg";
+import mp4 from "../assets/images/MP4.svg";
+import qml from "../assets/images/Qml.svg";
+import youtube from "../assets/images/youtube.svg";
+
+// background image 
+
+import bgpdf from "./../assets/images/bgPDF.svg";
+import bgepub from "../assets/images/bgEpub.svg";
+import bghtml from "../assets/images/bgHtml.svg";
+import bgmp4 from "../assets/images/bgMP4 .svg";
+import bgqml from "../assets/images/bgQml.svg";
+import bgyoutube from "../assets/images/bgYouTube.svg";
+
+
+
+
+
 export enum Role {
   STUDENT = "Student",
   TEACHER = "Teacher",
@@ -139,3 +160,43 @@ export enum ResourceType {
   LEARNER_POST_REQUISITE = "postrequisite",
   FACILITATOR_REQUISITE = "facilitator-requisite",
 }
+
+import { StaticImageData } from "next/image"; // Import StaticImageData for type safety with images
+
+export enum ContentType {
+  PDF = "application/pdf",
+  EPUB = "application/epub",
+  HTML = "application/html",
+  VIDEO_MP4 = "video/mp4",
+  QUESTION_SET = "application/vnd.sunbird.questionset",
+  H5P = "application/vnd.ekstep.h5p-archive",
+  YOUTUBE_VIDEO = "video/youtube",
+  YOUTUBE_X_VIDEO = "video/x-youtube",
+  WEBM_VIDEO = "video/webm",
+  VND = "application/vnd.ekstep.content-collection"
+}
+
+
+
+
+export type FileType = {
+  [key in ContentType]: {
+    name: string;
+    imgPath: StaticImageData;
+    BgImgPath?: StaticImageData;
+  };
+};
+
+// Create the mapping object with enum keys
+export const ContentCardsTypes: FileType = {
+  [ContentType.PDF]: { name: "PDF", imgPath: pdf, BgImgPath: bgpdf },
+  [ContentType.EPUB]: { name: "EPUB", imgPath: epub, BgImgPath: bgepub },
+  [ContentType.HTML]: { name: "HTML", imgPath: html, BgImgPath: bghtml },
+  [ContentType.VIDEO_MP4]: { name: "Video", imgPath: mp4, BgImgPath: bgmp4 },
+  [ContentType.QUESTION_SET]: { name: "Question Set", imgPath: qml, BgImgPath: bgqml },
+  [ContentType.H5P]: { name: "H5P", imgPath: html, BgImgPath: bghtml },
+  [ContentType.YOUTUBE_X_VIDEO]: { name: "YouTube", imgPath: youtube, BgImgPath: bgyoutube },
+  [ContentType.YOUTUBE_VIDEO]: { name: "YouTube", imgPath: youtube, BgImgPath: bgyoutube },
+  [ContentType.WEBM_VIDEO]: { name: "WEBM", imgPath: mp4, BgImgPath: bgmp4 },
+  [ContentType.VND]: { name: "WEBM", imgPath: html, BgImgPath: bghtml },
+};
