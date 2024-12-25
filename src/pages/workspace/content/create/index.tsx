0965f1741e7848dad371f88a1e7365d424e6a338
suvarnakale <suvarna.kale@tekditechnologies.com>
2024-12-25 12:47:01 +0530

    Issue #2925 feat : Add header for the workspace

--- src/pages/workspace/content/create/index.tsx
+++ src/pages/workspace/content/create/index.tsx
@@ -1,12 +1,35 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import React from "react";
 import dynamic from "next/dynamic";
-
+import Header from "@/components/layouts/header/Header";
+import { useTheme } from "@mui/material/styles";
+import { Box, useMediaQuery } from "@mui/material";
 // @ts-ignore
 const Create = dynamic(() => import("editor/Create"), { ssr: false });
 
 const create = () => {
-  return <Create />;
+  const [isSidebarOpen, setSidebarOpen] = React.useState(true);
+  const [isMobileSidebarOpen, setMobileSidebarOpen] = React.useState(false);
+  const theme = useTheme();
+  const lgUp = useMediaQuery(theme?.breakpoints?.up("lg"));
+
+  return (
+    <>
+      <Header
+        sx={{
+          zIndex: -1,
+          width: isSidebarOpen && lgUp ? "78%" : "100%",
+          paddingLeft: isSidebarOpen && lgUp ? "265px" : "",
+          backgroundColor: "#4d4639",
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+        }}
+        toggleMobileSidebar={() => setMobileSidebarOpen(true)}
+      />
+      <Box paddingTop={"3rem"}>
+        <Create />
+      </Box>
+    </>
+  );
 };
 
 export default create;
