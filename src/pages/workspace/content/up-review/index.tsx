778fe1fc2fb4089d561f643bbb60f5efb0f9b306
suvarnakale <suvarna.kale@tekditechnologies.com>
2024-12-31 13:26:59 +0530

    Issue #2925 feat : Add header for the workspace  - resolve comments

--- src/pages/workspace/content/up-review/index.tsx
+++ src/pages/workspace/content/up-review/index.tsx
@@ -1,33 +1,18 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import React from "react";
 import dynamic from "next/dynamic";
-import Header from "@/components/layouts/header/Header";
-import { useTheme } from "@mui/material/styles";
-import { Box, useMediaQuery } from "@mui/material";
+import { Box } from "@mui/material";
+import HeaderWrapper from "@/components/layouts/header/HeaderWrapper";
 
 // @ts-ignore
 // const Submitted = dynamic(() => import("editor/Submitted"), { ssr: false });
 const UpForReview = dynamic(() => import("editor/UpReview"), { ssr: false });
 
 const UpReview = () => {
-  const [isSidebarOpen, setSidebarOpen] = React.useState(true);
-  const [isMobileSidebarOpen, setMobileSidebarOpen] = React.useState(false);
-  const theme = useTheme();
-  const lgUp = useMediaQuery(theme?.breakpoints?.up("lg"));
-
   return (
     <>
-      <Header
-        sx={{
-          zIndex: -1,
-          width: isSidebarOpen && lgUp ? "78%" : "100%",
-          paddingLeft: isSidebarOpen && lgUp ? "265px" : "",
-          backgroundColor: "#4d4639",
-          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-        }}
-        toggleMobileSidebar={() => setMobileSidebarOpen(true)}
-      />
-      <Box paddingTop={"3rem"}>
+      <HeaderWrapper />
+      <Box paddingTop={"4rem"}>
         <UpForReview />
       </Box>
     </>
