15ecfd9afe39ad61339709d58bd1dbd7ea915f08
Vivek Kasture <vivek_kasture@techjoomla.com>
2025-02-11 18:55:54 +0530

    Issue #PS-0000 fix: Added Notification template management

--- src/components/notification-templates/TemplatePreview.tsx
+++ src/components/notification-templates/TemplatePreview.tsx
@@ -8,31 +8,21 @@ const TemplatePreview: React.FC<TemplatePreviewProps> = ({ template = "" }) => {
     return (
         <Box
             sx={{
-                padding: "16px",
-                // backgroundImage: "url('/images/mobile.png')",
-                 background:'white',
-                backgroundSize: "cover",
-                width: "100%",
-                maxWidth: "420px",
-                mt: 4,
-                borderRadius: "8px",
-                height: "600px",
-                overflowY: "auto",
-                overflowX: "hidden",
-            }}
-        >
-            <Typography variant="h5" sx={{ fontWeight: 600, mb: 2 }}>
-                Preview
-            </Typography>
-            <Box sx={{
                 height: "500px",
                 width: "400px",
                 margin: "10px",
                 marginBottom: "200px",
-            }}>
-
-            <div dangerouslySetInnerHTML={{ __html: template }} />
-            </Box>
+            }}
+        >
+            <div className="mobile-frame">
+                <div className="notch"></div>
+                <div className="screen">
+                    <div
+                        className="content-main"
+                        dangerouslySetInnerHTML={{ __html: template }}
+                    ></div>
+                </div>
+            </div>
         </Box>
     );
 };
